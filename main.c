c3dd200f8602528f5e4a910c019a2adec5894543
RobertoGalois <victor.vague@live.fr>
2017-08-08 17:03:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,51 +2,51 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 
-void pause()
+void    pause()
 {
-    signed short bool_continue = 1;
-    SDL_Event ev; 
+        signed short bool_continue = 1;
+        SDL_Event ev;
  
-    while (bool_continue)
-    {   
-        SDL_WaitEvent(&ev);
-        switch(ev.type)
+        while (bool_continue)
         {
-            case SDL_QUIT:
+            SDL_WaitEvent(&ev);
+            switch(ev.type)
+            {
+                case SDL_QUIT:
                 bool_continue = 0;
+            }
         }
-    }   
 }
 
-int main(int argc, char *argv[])
+int     main(int argc, char *argv[])
 {
-    SDL_Surface *window = NULL;
-    SDL_Surface *rectangle;
-    SDL_Rect position;
+        SDL_Surface *window = NULL;
+        SDL_Surface *rectangle;
+        SDL_Rect position;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
-    if (window == NULL) // Si l'ouverture a échoué, on le note et on arrête
-    {   
-        fprintf(stderr, "Impossible de charger le mode vidéo : %s\n",
-                SDL_GetError());
-        exit(EXIT_FAILURE);
-    }   
+        SDL_Init(SDL_INIT_VIDEO);
+        window = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+        if (window == NULL) // Si l'ouverture a échoué, on le note et on arrête
+        {
+            fprintf(stderr, "Impossible de charger le mode vidéo : %s\n",
+                    SDL_GetError());
+            exit(EXIT_FAILURE);
+        }
     
-    SDL_WM_SetCaption("Ma super fenêtre SDL !", NULL);
-    SDL_FillRect(window, NULL, SDL_MapRGB(window->format, 255, 255, 255));
+        SDL_WM_SetCaption("Ma super fenêtre SDL !", NULL);
+        SDL_FillRect(window, NULL, SDL_MapRGB(window->format, 255, 255, 255));
 
-    rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE, 220, 180, 32, 0, 0, 0, 0); 
-    position.x = 0;
-    position.y = 0;
+        rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE, 220, 180, 32, 0, 0, 0, 0);
+        position.x = 0;
+        position.y = 0;
 
-    SDL_FillRect(rectangle, NULL, SDL_MapRGB(window->format, 0, 0, 0));
-    SDL_BlitSurface(rectangle, NULL, window, &position);
+        SDL_FillRect(rectangle, NULL, SDL_MapRGB(window->format, 0, 0, 0));
+        SDL_BlitSurface(rectangle, NULL, window, &position);
 
-    SDL_Flip(window);
+        SDL_Flip(window);
 
-    pause();
-    SDL_Quit();
+        pause();
+        SDL_Quit();
 
-    return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 } 
